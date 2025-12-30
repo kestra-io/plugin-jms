38d0c36aa439198bb7c328a328c722985e3d7624
ben8t <pimpaudben@gmail.com>
2025-12-30 15:38:51 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/jms/package-info.java
+++ src/main/java/io/kestra/plugin/jms/package-info.java
@@ -1,7 +1,11 @@
 @PluginSubGroup(
     title = "JMS",
     description = "JMS messaging support plugins Trigger/Consumer/Producer",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    categories = PluginSubGroup.PluginCategory.MESSAGING,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.jms;
 
