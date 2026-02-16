6d43e042097361d2d9d1f4a4266b2dd90540c4bc
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-16 06:55:12 +0100

    chore: update plugin categories (#24)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    * chore: merge package categories attribute
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>
    Co-authored-by: Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>

--- src/main/java/io/kestra/plugin/jms/package-info.java
+++ src/main/java/io/kestra/plugin/jms/package-info.java
@@ -1,7 +1,9 @@
 @PluginSubGroup(
     title = "JMS",
-    description = "JMS messaging support plugins Trigger/Consumer/Producer",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    description = "JMS messaging support plugins Trigger/Consumer/Producer",categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.jms;
 
